ec6043c5340c3f347edd08a8dc3b43bd406a2a04
Syed Hasan Nawaz <syedhasannawaz05@gmail.com>
2026-03-24 13:00:34 +0500

    Week 1 Day 2: Env Variables and Docker Volumes

--- p.py
+++ p.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
+
 app=Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Day 1"
+	return os.getenv("Salam","Default")
 	
 if __name__=="__main__":
 	app.run(host="0.0.0.0",port=5000)
